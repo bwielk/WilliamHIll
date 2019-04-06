3f2250361a72f0e39a2016d8b526f06a5a2c5356
bwielk <bwielk@gmail.com>
2019-04-07 00:51:13 +0100

    The login page validation works

--- src/tests/WilliamHill.java
+++ src/tests/WilliamHill.java
@@ -1,7 +1,6 @@
 package tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,17 +10,16 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.awt.*;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 public class WilliamHill {
 
     private WebDriver driver;
     private WebDriverWait wait;
-    private WebElement foundWebElement;
 
     @BeforeMethod
     public void setUp(){
@@ -31,11 +29,11 @@ public class WilliamHill {
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.get("https://vegas.williamhill.com/");
-        wait = new WebDriverWait(driver, 10);
+        wait = new WebDriverWait(driver, 20);
     }
 
     @Test
-    public void searchForMayFairRoulette() throws InterruptedException, AWTException {
+    public void searchForMayFairRoulette() throws InterruptedException {
         Thread.sleep(10000);
         WebElement magnifierButton = driver.findElement(By.className("btn-search-magnifier"));
         magnifierButton.click();
@@ -48,6 +46,27 @@ public class WilliamHill {
         Actions actions = new Actions(driver);
         Action moveOverTheTile = actions.moveToElement(mayfairRouletteWidget).build();
         moveOverTheTile.perform();
-        WebElement detailsPanel = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#root > div > div.page.categories-list-page--home.categories-list-page.categories-list-page--tablet.page--tablet > div.categories-list-page__content.page__content > div:nth-child(3) > ul > li:nth-child(2) > div > div > div > div.gc-tile__content")))
+        WebElement moreButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("tile-menu__button--more")));
+        moreButton.click();
+        Thread.sleep(3000);
+        String playButtonSelector = "#root > div > div.page.search.search--tablet.page--tablet > section > div.tile-details " +
+                "> div > div.tile-details__left-content > div.tile-details__buttons > button.sc-EHOje.hGojEe";
+        try {
+            WebElement cookies = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("cookie-disclaimer__button")));
+            cookies.click();
+            Thread.sleep(2000);
+        }catch(NoSuchElementException e){
+            System.out.println("Cookie disclaimer hasn't showed up");
+        }
+        WebElement playButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(text(), 'Play Now')]")));
+//        List<WebElement> elementsInPanel = wait.until(ExpectedConditions.presenceOfNestedElementsLocatedBy(By.className("tile-details__left-content"), By.tagName("button")));
+//        for(int i=0; i< elementsInPanel.size(); i++){
+//            WebElement currentElement = elementsInPanel.get(i);
+//            System.out.println(currentElement.getText() + " " + currentElement.getLocation() + " " + currentElement.getTagName() +
+//                    currentElement.getAttribute("class"));
+//         }
+        playButton.click();
+        WebElement login = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("login-component__wrapper")));
+        Assert.assertNotNull(login);
     }
 }
